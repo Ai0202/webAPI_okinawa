786f033af4791bbb96b9b0cae2879604b181f4b0
Atsushi <>
2018-11-11 10:10:42 +0900

    qiita api完成

--- qiita/app.js
+++ qiita/app.js
@@ -4,15 +4,26 @@ $(function() {
   });
 
   function ajaxRequest() {
-    console.log(1);
-    $.ajax({
+    let tag = $('#name').val();
+    let url = 'https://qiita.com/api/v2/tags/'+ tag +'/items?page=1&per_page=10';
 
+    $.ajax({
+      url: url,
+      type: 'GET',
     })
     .done(function(data) {
-
+      renderResult(data);
     })
     .fail(function(error) {
 
     })
+  }
+
+  function renderResult(data) {
+    $('#result').text('');
+    for (item of data) {
+      let ancher = $('<a>').text(item.title).attr('href', item.url);
+      $('#result').append(ancher).append($('<br>'));
+    }
   }
 });
